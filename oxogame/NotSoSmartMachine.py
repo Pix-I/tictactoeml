55d995e938c719242ea381732258c01c7264b4e3
Jolan Stordeur <Pix@pix-labs.com>
2019-01-29 21:32:40 +0100

    Add smarter opponent

--- oxogame/NotSoSmartMachine.py
+++ oxogame/NotSoSmartMachine.py
@@ -16,11 +16,9 @@ def dumb_play(game_board):
 
 
 class Not_So_Smart_Machine:
-
     def __init__(self,reward,penalty):
         self.game_board = Oxo_board()
-        orig =  state(self.game_board)
-        self.states = dict([(orig.state,orig)])
+        self.total_games_played = 0
         self.victories = 0
         self.total_victories = 0
         self.losses = 0
@@ -32,54 +30,94 @@ class Not_So_Smart_Machine:
         self.reward = reward
         self.penalty = penalty
         orig =  state(self.game_board)
-        self.states = dict([(orig.state,orig)])
+        self.all_bubble_states = dict([(orig.state, orig)])
+        self.all_cross_states = dict([(orig.state, orig)])
         self.origin = orig
         self.fintesses = []
 
+    def reset_stats(self):
+        self.total_draws = 0
+        self.total_losses = 0
+        self.total_victories = 0
+        self.victories = 0
+        self.draws = 0
+        self.losses = 0
+
 
     def smarter_play(self,simulations):
-        print("Starting simulation: ", simulations , " runs planned with R: ", self.reward, " P",  self.penalty)
         #100 games
         for i in range(simulations):
-            self.play_many_games()
+            self.play_many_games('better_random')
+        self.print_sim_message(simulations)
+
+    def print_sim_message(self,simulations):
+        print("Starting simulation: ", simulations , " runs planned with R: ", self.reward, " P",  self.penalty)
         print("------------------------------------------")
         print("Simulation ended")
-        print("Games won:" , self.total_victories,"draws : ", self.total_draws, " losses: " ,self.total_losses)
-        print("Best fitness:",self.best_fitness)
+        print("Games won:", self.total_victories, "draws : ", self.total_draws, " losses: ", self.total_losses)
+        print("Best fitness:", self.best_fitness)
 
-    def play_many_games(self):
-        self.play_games(100)
+    def dumber_play(self,simulations):
+        #100 games
+        for i in range(simulations):
+            self.play_many_games('random')
+        self.print_sim_message(simulations)
+
+    def play_many_games(self,type):
+        if type== 'random':
+            self.play_random_games(100)
+        elif type == 'better_random':
+            self.play_less_random_games(100)
         self.overal_fitness = self.total_victories / (self.total_draws + self.total_losses + self.total_victories)
         print("Overall fitness: ", self.overal_fitness)
 
-    def play_games(self, number_of_games):
+    def play_random_games(self, number_of_games):
         for i in range(number_of_games):
-            game_states = dict()
+            self.total_games_played = self.total_games_played + 1
+            bubble_states = dict()
+            cross_states = dict()
             while self.game_board.has_move():
-                hasho = self.ultimate_bubble(game_states)
+                hasho = self.ultimate_bubble(bubble_states)
                 if self.game_board.check_oxo():
-                    self.states[hasho].win = True
-                    self.victories = self.victories + 1
-                    for winning_state in game_states:
-                        winning_state.apply_bonus(game_states[winning_state])
+                    self.apply_rewards(bubble_states, cross_states, 'bubble')
                     break
-                self.random_cross()
-                if self.game_board.check_oxo():
-                    for losing_state in game_states:
-                        losing_state.apply_penalty(game_states[losing_state])
-                        self.losses = self.losses + 1
-                    break
-            if not self.game_board.has_move():
-                for losing_state in game_states:
-                    losing_state.apply_penalty(game_states[losing_state])
-                    self.draws = self.draws + 1
-
+                if self.game_board.has_move():
+                    self.random_cross()
+                    hasho = ""
+                    if self.game_board.check_oxo():
+                        self.apply_rewards(cross_states,bubble_states,'cross')
+                        break
+            if not self.game_board.has_move() and not self.game_board.check_oxo():
+                self.end_draw(bubble_states,cross_states)
             self.game_board.reset()
+        self.end_simulation()
 
+    def play_less_random_games(self, number_of_games):
+        for i in range(number_of_games):
+            self.total_games_played = self.total_games_played + 1
+            bubble_states = dict()
+            cross_states = dict()
+            while self.game_board.has_move():
+                hasho = self.ultimate_bubble(bubble_states)
+                if self.game_board.check_oxo():
+                    self.apply_rewards(bubble_states, cross_states, 'bubble')
+                    break
+                if self.game_board.has_move():
+                    self.ultimate_cross(cross_states)
+                    hasho = ""
+                    if self.game_board.check_oxo():
+                        self.apply_rewards(cross_states,bubble_states,'cross')
+                        break
+            if not self.game_board.has_move() and not self.game_board.check_oxo():
+                self.end_draw(bubble_states,cross_states)
+            self.game_board.reset()
+        self.end_simulation()
+
+    def end_simulation(self):
         self.adjustRewards()
         fitness = self.victories / (self.losses + self.victories + self.draws)
         self.fintesses.append(fitness)
-        if fitness> self.best_fitness:
+        if fitness > self.best_fitness:
             self.best_fitness = fitness
             print("New best fitness level!", fitness)
         self.total_victories = self.total_victories + self.victories
@@ -88,6 +126,24 @@ class Not_So_Smart_Machine:
         self.victories = 0
         self.draws = 0
         self.losses = 0
+
+    def end_draw(self, game_states,cross_states):
+        self.draws = self.draws + 1
+        for bubble in game_states:
+            bubble.apply_penalty(game_states[bubble])
+        for cross in cross_states:
+            cross.apply_win(cross_states[cross])
+
+
+    def apply_rewards(self, winner, loser, hasho):
+        if hasho == 'bubble':
+            self.victories = self.victories + 1
+        else:
+            self.losses = self.losses +1
+        for winning_state in winner:
+            winning_state.apply_win(winner[winning_state])
+        for losing_state in loser:
+            losing_state.apply_penalty(loser[losing_state])
 
     def ultimate_bubble(self, game_states):
         current_hash = self.game_board.get_hash()
@@ -103,19 +159,40 @@ class Not_So_Smart_Machine:
         game_states[current_state]  = move_number
         return current_hash
 
+    def ultimate_cross(self, game_states):
+        current_hash = self.game_board.get_hash()
+        current_state = self.get_cross_state(current_hash)
+        move_number = 0
+        while current_hash == self.game_board.get_hash():
+            move_number = current_state.move()
+            self.smart_cross(move_number)
+            if current_hash == self.game_board.get_hash():
+                current_state.disable_move_number(move_number)
+        game_states[current_state]  = move_number
+        return current_hash
+
+
+    def get_cross_state(self, current_hash):
+        if current_hash not in self.all_cross_states:
+            new_state = state(self.game_board)
+            self.all_cross_states[new_state.state] = new_state
+        return self.all_cross_states[current_hash]
 
 
     def get_state(self, current_hash):
-        if current_hash not in self.states:
+        if current_hash not in self.all_bubble_states:
             new_state = state(self.game_board)
-            self.states[new_state.state] = new_state
-        return self.states[current_hash]
+            self.all_bubble_states[new_state.state] = new_state
+        return self.all_bubble_states[current_hash]
 
     def smart_bubble(self, numb):
         x = numb%3 + 1
         y = int(numb/3) +1
         self.game_board.place_bubble(x,y)
-
+    def smart_cross(self, numb):
+        x = numb%3 + 1
+        y = int(numb/3) +1
+        self.game_board.place_cross(x,y)
     def random_cross(self):
         hasho = self.game_board.get_hash()
         while self.game_board.get_hash() == hasho and self.game_board.has_move():
@@ -123,7 +200,7 @@ class Not_So_Smart_Machine:
         return hasho
 
     def adjustRewards(self):
-        for key,st in self.states.items():
+        for key,st in self.all_bubble_states.items():
             st.reward = self.reward
             st.penalty = self.penalty
 
@@ -154,7 +231,7 @@ class state:
         self.moves[move_number] = self.moves[move_number] - self.penalty
         if(self.moves[move_number])<0:
             self.moves[move_number] = 0
-    def apply_bonus(self,move_number):
+    def apply_win(self, move_number):
         for m in range(8):
             if m == move_number:
                 self.moves[m] = self.moves[move_number] + self.reward
@@ -162,4 +239,7 @@ class state:
                 self.moves[m] = int(self.moves[m] - self.penalty/2)
                 if(self.moves[m])<0:
                     self.moves[m] = 0
+    def apply_draw(self):
+        for m in range(8):
+            self.moves[m] = self.moves[m] + randint(-self.penalty,self.reward)
 
--- oxogame/pixoplot.py
+++ oxogame/pixoplot.py
@@ -2,43 +2,132 @@ from oxogame.NotSoSmartMachine import Not_So_Smart_Machine
 import plotly.plotly as py
 import plotly.graph_objs as go
 import time
-secondsArray = []
 
-demo_machine = Not_So_Smart_Machine(5,3)
-seconds = time.time()
+config={'responsive': True}
 
-for i in range(10):
-    secondsArray.append(time.time()-seconds)
-    demo_machine.smarter_play(100)
-print(secondsArray)
-
-arrayp = []
-for i in range(1,9):
-    arrayp.append(secondsArray[i] - secondsArray[i-1])
-print(arrayp)
-print(demo_machine.origin.moves)
-
-trace0 = go.Scatter(
-    x=[1, 2, 3, 4,5,6,7,8,9],
-    y=demo_machine.fintesses
-)
-data = [trace0]
-
-py.plot(data, filename = 'fitness', auto_open=False)
-
-trace0 = go.Scatter(
-    x=[1, 2, 3, 4,5,6,7,8,9],
-    y=arrayp
-)
-data = [trace0]
-
-py.plot(data, filename = 'timing', auto_open=True)
+def first_graphs():
+    secondsArray = []
+    demo_machine = Not_So_Smart_Machine(5, 3)
+    seconds = time.time()
+    for i in range(10):
+        secondsArray.append(time.time() - seconds)
+        demo_machine.dumber_play(100)
+    print(secondsArray)
+    arrayp = []
+    for i in range(1, 9):
+        arrayp.append(secondsArray[i] - secondsArray[i - 1])
+    print(arrayp)
+    print(demo_machine.origin.moves)
+    trace0 = go.Scatter(
+        x=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+        y=arrayp
+    )
+    data = [trace0]
+    py.plot(data,config = config, output_type='div',
+            include_plotlyjs='cdn',filename='timing', auto_open=True)
+    data = [go.Bar(
+        x=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+        y=demo_machine.origin.moves
+    )]
+    py.plot(data,config = config,output_type='div',
+            include_plotlyjs='cdn', filename='weights', auto_open=True)
 
 
+first_graphs()
 
-data = [go.Bar(
-    x=[0,1, 2, 3, 4,5,6,7,8],
-    y=demo_machine.origin.moves
-)]
+def demo_play():
+    demo_machine = Not_So_Smart_Machine(5, 3)
+    result_set = []
+    for i in range(8):
+        demo_machine.dumber_play(1000)
+        result_set.append(demo_machine.overal_fitness)
+    trace0 = go.Scatter(
+        x=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+        y=result_set
+    )
+    data = [trace0]
+    py.plot(data,config = config, filename='demo_play', auto_open=True)
 
-py.plot(data, filename = 'weights', auto_open=False)
+
+demo_play()
+
+def random_fitness():
+    fitness_machine = Not_So_Smart_Machine(5, 3)
+    wins = []
+    losses = []
+    draws = []
+    xgames = []
+    for i in range(8):
+        xgame = "{} simulations".format(100 * (i + 1))
+        xgames.append(xgame)
+        fitness_machine.dumber_play(100)
+        wins.append(fitness_machine.total_victories)
+        losses.append(fitness_machine.total_losses)
+        draws.append(fitness_machine.total_draws)
+        fitness_machine.reset_stats()
+    print(fitness_machine.total_games_played)
+    print(xgames)
+    winsBar = go.Bar(
+        x=xgames,
+        y=wins,
+        name='Wins'
+    )
+    drawsBar = go.Bar(
+        x=xgames,
+        y=draws,
+        name='Draws'
+    )
+    lossesBar = go.Bar(
+        x=xgames,
+        y=losses,
+        name='Losses'
+    )
+    data = [winsBar, drawsBar, lossesBar]
+    layout = go.Layout(
+        barmode='stack'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig,config = config, filename='random_fitness')
+
+def less_random_fitness():
+    fitness_machine = Not_So_Smart_Machine(5, 3)
+    wins = []
+    losses = []
+    draws = []
+    xgames = []
+    for i in range(8):
+        xgame = "{} simulations".format(100 * (i + 1))
+        xgames.append(xgame)
+        fitness_machine.smarter_play(100)
+        wins.append(fitness_machine.total_victories)
+        losses.append(fitness_machine.total_losses)
+        draws.append(fitness_machine.total_draws)
+        fitness_machine.reset_stats()
+    print(fitness_machine.total_games_played)
+    print(xgames)
+    winsBar = go.Bar(
+        x=xgames,
+        y=wins,
+        name='Wins'
+    )
+    drawsBar = go.Bar(
+        x=xgames,
+        y=draws,
+        name='Draws'
+    )
+    lossesBar = go.Bar(
+        x=xgames,
+        y=losses,
+        name='Losses'
+    )
+    data = [winsBar, drawsBar, lossesBar]
+    layout = go.Layout(
+        barmode='stack'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig,config = config, filename='less_random_fitness')
+
+
+random_fitness()
+less_random_fitness()
+
